c7fcf79b170b874b4c08974a06c0dd7acd516f08
mahteixeira <teixeiramarina13@gmail.com>
2023-03-09 16:09:46 -0300

    model

---
+++ Model/EnderecoModel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Api_Cep\Model;
+
+use Api_Cep\DAO\EnderecoDAO;
+use Exception;
+
+class EnderecoModel extends Model
+{
+    public $id_logradouro, $tipo, $descricao, $id_cidade, $uf, $complemento, $descricao_sem_numero,
+           $descricao_cidade, $codigo_cidade_ibge, $descricao_bairro;
+
+    public $arr_cidades;
+
+    public function getLogradouroByBairroAndCidade(string $bairro, int $id_cidade)
+    {
+        try
+        {
+            $dao = new EnderecoDAO();
+
+            $this->rows = $dao->selectLogradouroByBairroAndCidade($bairro,$id_cidade);
+        } catch(Exception $e) {
+            throw $e;
+        }
+    }
+}
